ddca3e85de8a374d827b6216da055314a7351458
rerere1226 <kuinie12@icloud.com>
2025-03-04 09:37:37 +0800

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+//placeのスライドインと固定
+// document.addEventListener("DOMContentLoaded", function () {
+//     const sections = document.querySelectorAll(".place-content"); // place-content だけアニメーション
+
+//     const observer = new IntersectionObserver(entries => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add("show"); // クラスを追加して表示
+//             }
+//         });
+//     }, { threshold: 0.2 });
+
+//     sections.forEach(section => {
+//         observer.observe(section);
+//     });
+// });
+
